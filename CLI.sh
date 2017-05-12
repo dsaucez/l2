8309bbdc4651f9b715ac06ced5061a2951c64e4e
Damien Saucez <damien.saucez@inria.fr>
2017-05-12 12:16:40 +0200

    no need anymore

--- CLI.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-source $THIS_DIR/../../env.sh
-
-CLI_PATH=$BMV2_PATH/targets/simple_switch/sswitch_CLI
-
-$CLI_PATH --thift-ip 127.0.0.1 --thrift-port 45001 counter.json
