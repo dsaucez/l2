eee6287b69978326ee1b2bfb1f88ec60a73049f6
Damien Saucez <damien.saucez@inria.fr>
2017-06-27 16:10:49 +0200

    test g5k

--- g5k/simple.rb
+++ g5k/simple.rb
@@ -1,50 +1,100 @@
 #!/usr/bin/ruby
 require 'distem'
 
-img_p4 = "file:///home/dsaucez/p4/p4-tornado-networkx-lxc-img.tar.gz"
+img_p4 = "file:///home/dsaucez/p4/p4-tornado-lxc-img.tar.gz"
 img = "file:///home/dsaucez/p4/client-lxc-img.tar.gz"
 hosts = ARGV[0].split(',')
 
 
 Distem.client { |cl|
 
-  cl.vnetwork_create('adm', '220.0.0.0/8', {'network_type' => 'vxlan'})
 
-  cl.vnetwork_create('vnet10-11', '10.42.0.0/16', {'network_type' => 'vxlan'})
-  cl.vnetwork_create('vnet10-12', '10.42.0.0/16', {'network_type' => 'vxlan'})
+
+  puts "Creat networks..."
   
-  nodes = ['h11', 'h12', 's10']
-  cl.vnode_create('h11',
+  # admin network
+  cl.vnetwork_create('adm', '220.0.0.0/8', {'network_type' => 'vxlan'})
+ 
+  # control network
+  cl.vnetwork_create('ctrlnet', '10.164.0.0/24', {'network_type' => 'vxlan'})
+
+  # data networks
+  cl.vnetwork_create('s10net', '10.164.10.0/24', {'network_type' => 'vxlan'})
+  cl.vnetwork_create('s20net', '10.164.20.0/24', {'network_type' => 'vxlan'})
+  cl.vnetwork_create('s10-s20net', '10.164.120.0/24', {'network_type' => 'vxlan'})
+
+  # Create the nodes
+  h = 0
+  nodes = [ ]
+  node = 'controller'
+  nodes.push(node)
+  cl.vnode_create(node,
          {
-           'host' => hosts[0],
-           'vfilesystem' =>{'image' => img,'shared' => true},
+           'host' => hosts[h],
+           'vfilesystem' =>{'image' => img_p4,'shared' => true},
            'vifaces' => [
-                         {'name' => 'ifadm', 'vnetwork' => 'adm', 'address' => '220.0.0.11'},
-                         {'name' => 'if0', 'vnetwork' => 'vnet10-11', 'address' => '10.42.11.11', 'macaddress' => '1e:de:ad:10:11:11'},
+                         {'name' => 'ifadm', 'vnetwork' => 'adm', 'address' => '220.0.0.1'},
+                         {'name' => 'if0', 'vnetwork' => 'ctrlnet', 'address' => '10.164.0.1'},
                         ]
          })
-   cl.vnode_create('h12',
+  
+  h += 1
+  node = 's10'
+  nodes.push(node)
+  cl.vnode_create(node,
          {
-           'host' => hosts[1],
-           'vfilesystem' =>{'image' => img,'shared' => true},
-           'vifaces' => [
-                         {'name' => 'ifadm', 'vnetwork' => 'adm', 'address' => '220.0.0.12'},
-                         {'name' => 'if0', 'vnetwork' => 'vnet10-12', 'address' => '10.42.12.12', 'macaddress' => '1e:de:ad:10:12:12'},
-                        ]
-         })
-
-
-  cl.vnode_create('s10',
-         {
-           'host' => hosts[2],
+           'host' => hosts[h],
            'vfilesystem' =>{'image' => img_p4,'shared' => true},
            'vifaces' => [
                          {'name' => 'ifadm', 'vnetwork' => 'adm', 'address' => '220.0.0.10'},
-                         {'name' => 'if11', 'vnetwork' => 'vnet10-11', 'address' => '10.42.11.10', 'macaddress' => '1e:de:ad:10:11:10'},
-                         {'name' => 'if12', 'vnetwork' => 'vnet10-12', 'address' => '10.42.12.10', 'macaddress' => '1e:de:ad:10:12:10'},
+                         {'name' => 'if0', 'vnetwork' => 'ctrlnet', 'address' => '10.164.0.10'},
+                         {'name' => 'if1', 'vnetwork' => 's10-s20net', 'address' => '10.164.120.10', 'macaddress' => '1e:de:ad:10:20:10'},
+                         {'name' => 'if2', 'vnetwork' => 's10net', 'address' => '10.164.10.10', 'macaddress' => '1e:de:ad:10:00:10'},
+                        ]
+         })
+  
+  h += 1
+  node = 's20'
+  nodes.push(node)
+  cl.vnode_create(node,
+         {
+           'host' => hosts[h],
+           'vfilesystem' =>{'image' => img_p4,'shared' => true},
+	   'vifaces' => [
+                         {'name' => 'ifadm', 'vnetwork' => 'adm', 'address' => '220.0.0.20'},
+                         {'name' => 'if0', 'vnetwork' => 'ctrlnet', 'address' => '10.164.0.20'},
+                         {'name' => 'if1', 'vnetwork' => 's10-s20net', 'address' => '10.164.120.20', 'macaddress' => '1e:de:ad:10:20:20'},
+                         {'name' => 'if2', 'vnetwork' => 's20net', 'address' => '10.164.20.20', 'macaddress' => '1e:de:ad:20:00:20'},
+                        ]
+         })
+  
+  h += 1
+  node = 'h11'
+  nodes.push(node)
+  cl.vnode_create(node,
+         {
+           'host' => hosts[h],
+           'vfilesystem' =>{'image' => img,'shared' => true},
+           'vifaces' => [
+                         {'name' => 'ifadm', 'vnetwork' => 'adm', 'address' => '220.0.0.11'},
+                         {'name' => 'if1', 'vnetwork' => 's10net', 'address' => '10.164.10.11', 'macaddress' => '1e:de:ad:10:00:11'},
+                        ]
+         })
+   
+  h += 1
+  node = 'h21'
+  nodes.push(node)
+  cl.vnode_create(node,
+         {
+           'host' => hosts[h],
+           'vfilesystem' =>{'image' => img,'shared' => true},
+	   'vifaces' => [
+                         {'name' => 'ifadm', 'vnetwork' => 'adm', 'address' => '220.0.0.21'},
+                         {'name' => 'if1', 'vnetwork' => 's20net', 'address' => '10.164.20.21', 'macaddress' => '1e:de:ad:20:00:21'},
                         ]
          })
 
+  puts nodes
 
   puts "Starting vnodes..."
   cl.vnodes_start(nodes)
@@ -56,10 +106,20 @@ Distem.client { |cl|
     cl.set_global_etchosts
 
     puts "Setting the nodes..."
-    # We Add a default route to h11
-    cl.vnode_execute('h11', "ip route add 0.0.0.0/0 via 10.10.11.10")
-    # We Add a default route to h12
-    cl.vnode_execute('h12', "ip route add 0.0.0.0/0 via 10.10.12.10")
+    # Add a route to s20 network on s10 via s20
+    cl.vnode_execute('s10', "ip route add 10.164.20.0/24 via 10.164.120.20")
+    # Add a route to s20 network on h11 via s10
+    cl.vnode_execute('h11', "ip route add 10.164.20.0/24 via 10.164.10.10")
+
+    # Add a route to s10 network on s20 via s10
+    cl.vnode_execute('s20', "ip route add 10.164.10.0/24 via 10.164.120.10")
+    # Add a route to s10 network on h21 via s20
+    cl.vnode_execute('h21', "ip route add 10.164.10.0/24 via 10.164.20.20")
+
+    # Activate forwarding on s10 and s20
+    cl.vnode_execute('s10', "sysctl -w net.ipv4.ip_forward=1")
+    cl.vnode_execute('s20', "sysctl -w net.ipv4.ip_forward=1")
+
     puts "Done"
   else
     puts "vnodes are unreachable"
--- g5k/start_switch.sh
+++ g5k/start_switch.sh
@@ -3,4 +3,4 @@
 # compile P4
 p4c-bmv2 ../p4src/l2.p4 --json l2.json
 # start switch
-simple_switch --log-console -i 1@if11 -i 2@if12 -i 11@cpu-veth-1 --thrift-port 45010 l2.json
+simple_switch --log-console -i 1@if1 -i 2@if2 -i 11@cpu-veth-1 l2.json
